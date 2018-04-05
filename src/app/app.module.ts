7379aef8ceb91fef0f32a36ad8bb5d1fc4b98b64
ouxiang <haoruishen_2010@163.com>
2018-04-05 21:58:44 +0800

    Angular-DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Product1Component } from './product1/product1.component';
+import { ProductService } from './shared/product.service';
+import { Product2Component } from './product2/product2.component';
+import { LoggerService } from './shared/logger.service';
+import { AnotherProductService } from './shared/another-product.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Product1Component,
+    Product2Component
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: ProductService,
+
+    useFactory: (logger: LoggerService, isDev) => {
+      if (isDev) {
+        return new ProductService(logger);
+      } else {
+        return new AnotherProductService(logger)
+      }
+    },   // 工厂方法是单例方法
+    // useFactory: (logger: LoggerService, appConfig) => {
+    //   if (appConfig.isDev) {
+    //     return new ProductService(logger);
+    //   } else {
+    //     return new AnotherProductService(logger)
+    //   }
+    // },
+    deps: [LoggerService, 'IS_DEV_ENV']
+    // deps: [LoggerService, 'IS_CONFIG']
+  }, LoggerService,
+  {
+    provide: 'IS_DEV_ENV', useValue: false
+    // provide: 'IS_CONFIG', useValue: {isDev:false}
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { LoggerService } from './logger.service';
+
+// 装饰器 
+@Injectable()
+export class ProductService {
+
+  constructor(public logger: LoggerService) { }
+  getProduct(): Product {
+    this.logger.log("getProduct方法被调用")
+    return new Product(0, 'iPhone8', 5899, '最新款苹果手机')
+  }
+
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public desc: string
+  ) {
+
+  }
+}
